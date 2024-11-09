2751d9530cdc3a6975cc12c093dde200f8befd6a
Zainul khan <zainulkhan032@gmail.com>
2024-11-09 18:58:48 +0530

    more optimize

--- Router/src/Components/Github/Github.jsx
+++ Router/src/Components/Github/Github.jsx
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from "react";
-
+import { useLoaderData } from "react-router-dom";
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/harshadekawade")
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+  const data = useLoaderData();
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/harshadekawade")
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data));
+  // }, []);
   return (
     <>
       <h2>Github Followers: {data.followers}</h2>
       <div>
-        <img src = {data.avatar_url} alt="github img" />
+        <img src={data.avatar_url} alt="github img" />
       </div>
     </>
   );
 }
 export default Github;
+
+
+// better way
+export const GitHub = async () => {
+  const response = await fetch("https://api.github.com/users/harshadekawade");
+  return response.json();
+};
--- Router/src/main.jsx
+++ Router/src/main.jsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements , Route } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Home from "./Components/Home/Home.jsx";
 import About from "./Components/About/About.jsx";
 import Contact from "./Components/Contact/Contact.jsx";
 import User from "./Components/User/User.jsx";
-import Github from "./Components/Github/Github.jsx";
+import Github, { GitHub } from "./Components/Github/Github.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -32,15 +37,16 @@ import Github from "./Components/Github/Github.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path = '/' element = {<App/>}>
-      <Route path = '' element = {<Home/>}/>
-      <Route path = 'about' element = {<About />}/>
-      <Route path = 'contact' element = {<Contact />}/>
-      <Route path = 'user/:userid' element = {<User />}/>
-      <Route path = 'github' element = {<Github />}/>
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      {/* <Route path="github" element={<Github />} /> */}
+      <Route loader={GitHub} path="github" element={<Github />} />
     </Route>
   )
-)
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
